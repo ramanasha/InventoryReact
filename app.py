db72529a3410c7f9057ce2bc29e3e7c85f8c12c5
sams <sams@debian>
2015-09-09 16:20:46 +0530

    products list api created

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from mongoengine import *
+import json
 
-main_inventory= connect('inventory')
+app = Flask(__name__)
+
+connect('inventory')
 
 item_category = ('industrial','commercial','garments','electronics')
 
 class Item(Document):
-    
     item_name = StringField()
     item_sku = StringField()
     item_type = StringField(choices= item_category)
     item_stockable = BooleanField()
     item_price = FloatField()
     item_instock = BooleanField()
+
+user_category = ('admin','user')
+    
+class User(Document):
+    user_name = StringField()
+    user_email = StringField()
+    user_pswd = StringField()
+    user_type = StringField(choices= user_category)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/products')
+def products():
+    products = Item.objects.all()
+    final_products = { 'products': products }
+    return products.to_json()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+        
