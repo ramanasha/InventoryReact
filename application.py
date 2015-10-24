a03a1df1888c6a6f9971cbcc45886032a29a52db
salmansaleemkma <salman.saleemkma@gmail.com>
2015-10-24 09:12:55 +0530

    pep8 style check

--- application.py
+++ application.py
@@ -6,31 +6,30 @@ from flask.ext.cors import CORS
 application = Flask(__name__)
 cors = CORS(application)
 
-<<<<<<< HEAD
 connect('inventory')
-=======
-connect('inventory',host='54.179.172.239', port=27017)
 
->>>>>>> a4ab189507b88bff7af4b68bf91933c2ff2b7405
-item_category = ('industrial','commercial','garments','electronics')
+item_category = ('industrial', 'commercial', 'garments', 'electronics')
+
 
 class Item(Document):
     item_name = StringField()
     item_description = StringField()
     item_sku = StringField()
-    item_type = StringField(choices= item_category)
+    item_type = StringField(choices=item_category)
     item_stockable = BooleanField(default=False)
     item_price = FloatField()
     item_instock = BooleanField(default=False)
     item_units = IntField()
 
-user_category = ('admin','user')
-    
+user_category = ('admin', 'user')
+
+
 class User(Document):
     user_name = StringField()
     user_email = StringField()
     user_pswd = StringField()
-    user_type = StringField(choices= user_category)
+    user_type = StringField(choices=user_category)
+
 
 @application.route('/')
 @application.route('/index.html')
@@ -38,17 +37,19 @@ class User(Document):
 def index():
     return render_template('index.html')
 
+
 @application.route('/products')
 def products():
     products = Item.objects.all()
     return products.to_json()
+
 
 @application.route('/products/<item_id>')
 def get_product(item_id):
     item = Item.objects.get(id=item_id)
     return item.to_json()
 
+
 if __name__ == '__main__':
     application.debug = True
     application.run()
-        
