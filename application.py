5a462987bfd0e106e242a5d056c07ae800b3c8c8
salmansaleemkma <salman.saleemkma@gmail.com>
2015-10-31 22:18:08 +0530

    connecting to MongoLab

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from flask.ext.cors import CORS
 application = Flask(__name__)
 cors = CORS(application)
 
-connect('inventory')
+connect('inventory', host='ds045054.mongolab.com', port=45054, username='inventory_user', password='GameHalt')
 
 item_category = ('industrial', 'commercial', 'garments', 'electronics')
 
@@ -38,16 +38,31 @@ def index():
     return render_template('index.html')
 
 
-@application.route('/products')
+@application.route('/products', methods=['GET'])
 def products():
     products = Item.objects.all()
     return products.to_json()
 
 
-@application.route('/products/<item_id>')
+@application.route('/products/<item_id>', methods=['GET'])
 def get_product(item_id):
     item = Item.objects.get(id=item_id)
     return item.to_json()
+
+@application.route('/products/new', methods=['POST'])
+def new_product():
+    new_item = Item(item_name=request.json['product']['item_name'], \
+                    item_description=request.json['product']['item_description'], \
+                    item_sku=request.json['product']['item_sku'], \
+                    item_type=request.json['product']['item_type'], \
+                    item_stockable=request.json['product']['item_stockable'], \
+                    item_price=request.json['product']['item_price'], \
+                    item_instock=request.json['product']['item_instock'], \
+                    item_units=request.json['product']['item_units'])
+
+    new_item.save()
+    return new_item.to_json(), 201
+                    
 
 
 if __name__ == '__main__':
